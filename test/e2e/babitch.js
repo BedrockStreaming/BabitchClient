a3d2e5ef5da6f39f4adef23b29ebad8d8b97bdd6
Kenny Dits <kenny.dits@m6.fr>
2013-12-17 13:19:49 +0100

    fix tests and add all goals tests

--- test/e2e/babitch.js
+++ test/e2e/babitch.js
@@ -2,7 +2,7 @@
 
 /* http://docs.angularjs.org/guide/dev_guide.e2e-testing */
 
-describe('Babitch', function() {
+describe('Babitch : Choose player', function() {
 
 	beforeEach(function() {
 		browser().navigateTo('/');
@@ -51,21 +51,79 @@ describe('Babitch', function() {
   		expect(element('.score ul li:eq(1)').text()).toBe('0');
 	});
 
-	it('should add a goal for blue team if the blue defender goal',function() {
-		var player = {team: "blue", position: "defense", player_id: 0};
-		using('li.blue.defense').element(':button:eq(0)').click(player);
-		sleep(3);
-  		expect(element('.score ul li:eq(0)').text()).toBe('0');
-		expect(element('.score ul li:eq(1)').text()).toBe('1');
+	
 
+});
+
+describe('Babitch : Game', function() {
+
+	beforeEach(function() {
+		browser().navigateTo('/');
+		
+		//Choose 4 players
+		using('li.blue.attack').select('player.player_id').option('0');
+		using('li.blue.defense').select('player.player_id').option('1');
+		using('li.red.attack').select('player.player_id').option('2');
+		using('li.red.defense').select('player.player_id').option('3');
+
+		//Begin a match
+		element('.actionStart :button').click();
 	});
 
+	//Defense goal
+	it('should add a goal for the blue team if the blue defender goal',function() {
+		var player = {team: "blue", position: "defense", player_id: 1};
+		using('li.blue.defense').element(':button:eq(0)').click(player);
+  		expect(element('.score ul li:eq(0)').text()).toBe('0');
+		expect(element('.score ul li:eq(1)').text()).toBe('1');
+	});
+
+	it('should add a goal for the red team if the red defender goal',function() {
+		var player = {team: "red", position: "defense", player_id: 3};
+		using('li.red.defense').element(':button:eq(0)').click(player);
+  		expect(element('.score ul li:eq(0)').text()).toBe('1');
+		expect(element('.score ul li:eq(1)').text()).toBe('0');
+	});
+
+	//Defense CSC
 	it('should add a goal for red team if the blue defender goal csc',function() {
 		using('li.blue.defense').element(':button:eq(1)').click();
   		expect(element('.score ul li:eq(0)').text()).toBe('1');
+		expect(element('.score ul li:eq(1)').text()).toBe('0');
+	});
+
+	it('should add a goal for blue team if the red defender goal csc',function() {
+		using('li.red.defense').element(':button:eq(1)').click();
+  		expect(element('.score ul li:eq(0)').text()).toBe('0');
 		expect(element('.score ul li:eq(1)').text()).toBe('1');
+	});
 
+	//Attack Goal
+	it('should add a goal for the blue team if the blue attacker goal',function() {
+		var player = {team: "blue", position: "attack", player_id: 0};
+		using('li.blue.attack').element(':button:eq(0)').click(player);
+  		expect(element('.score ul li:eq(0)').text()).toBe('0');
+		expect(element('.score ul li:eq(1)').text()).toBe('1');
+	});
 
+	it('should add a goal for the red team if the red attacker goal',function() {
+		var player = {team: "red", position: "attack", player_id: 2};
+		using('li.red.defense').element(':button:eq(0)').click(player);
+  		expect(element('.score ul li:eq(0)').text()).toBe('1');
+		expect(element('.score ul li:eq(1)').text()).toBe('0');
+	});
+
+	//Attack CSC
+	it('should add a goal for red team if the blue attacker goal csc',function() {
+		using('li.blue.attack').element(':button:eq(1)').click();
+  		expect(element('.score ul li:eq(0)').text()).toBe('1');
+		expect(element('.score ul li:eq(1)').text()).toBe('0');
+	});
+
+	it('should add a goal for blue team if the red attacker goal csc',function() {
+		using('li.red.attack').element(':button:eq(1)').click();
+  		expect(element('.score ul li:eq(0)').text()).toBe('0');
+		expect(element('.score ul li:eq(1)').text()).toBe('1');
 	});
 
 });
