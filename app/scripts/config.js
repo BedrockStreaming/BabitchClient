db503b4c5fadb39f2de48d7d52001da262192825
Morgan BRUNOT <mbrunot.externe@m6.fr>
2013-12-18 08:53:52 +0100

    change ws url

--- app/scripts/config.js
+++ app/scripts/config.js
@@ -2,5 +2,5 @@
 
 var babitchServer = angular.module("babitchServer", [])
     .constant('CONFIG', {
-        'BABITCH_WS_URL': "http://baby.cytron.fr/v1"
+        'BABITCH_WS_URL': "http://127.0.0.1:8081/app_dev.php/v1"
 });
