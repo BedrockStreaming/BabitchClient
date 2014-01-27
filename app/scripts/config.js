304efb199ebead1fcd9a5fc036efa00b805c4ff8
Nicolas Bazille <oltodo@msn.com>
2014-01-27 09:07:15 +0100

    Remove an other personnal config

--- app/scripts/config.js
+++ app/scripts/config.js
@@ -2,7 +2,7 @@
 
 var babitchServer = angular.module("babitchServer", [])
     .constant('CONFIG', {
-        'BABITCH_WS_URL': "http://127.0.0.1:8888/app_dev.php/v1",
+        'BABITCH_WS_URL': "http://127.0.0.1:8081/app_dev.php/v1",
         'BABITCH_LIVE_FAYE_URL' :'http://faye-babitch.herokuapp.com/faye',
-        'BABITCH_LIVE_FAYE_CHANNEL' :'/nbazille'
+        'BABITCH_LIVE_FAYE_CHANNEL' :'/test-channel-to-replace'
 });
