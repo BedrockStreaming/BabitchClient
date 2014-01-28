ec3bc435e9cb60ae4ba4b4fa68a7340ec0d4a575
Kenny Dits <kenny.dits@m6.fr>
2014-01-28 06:37:36 +0100

    add filters prettyTime and gravatarize with test

--- app/scripts/controllers/babitch.js
+++ app/scripts/controllers/babitch.js
@@ -359,30 +359,4 @@ babitchFrontendApp.controller("babitchCtrl", function ($scope, $http, CONFIG, fa
             }
         });
     };
-})
-.filter('prettyTime', function() {
-    return function(time) {
-        var minutes = Math.floor(time/60);
-        var seconds = time%60;
-
-        minutes = (minutes < 10 ? '0'+minutes : minutes);
-        seconds = (seconds < 10 ? '0'+seconds : seconds);
-
-        return minutes+' : '+seconds;
-    }
-})
-.filter('gravatarize', function(gravatarService) {
-    return function(email, size) {
-        if(!email) {
-            return gravatarService.url('', {
-                default: 'mm',
-                size: size
-            });
-        }
-
-        return gravatarService.url(email, {
-            default: 'wavatar',
-            size: size
-        });
-    }
 });
