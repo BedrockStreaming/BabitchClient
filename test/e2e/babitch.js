2028c0f17326e0f5220d9c22d46e870a4fdc5f2a
Nicolas Chaulet <n.chaulet@gmail.com>
2014-01-28 21:54:17 +0100

    init protractor use and use PageObject pattern for e2e test

--- test/e2e/babitch.js
+++ test/e2e/babitch.js
@@ -1,172 +1,210 @@
 'use strict';
 
 /* http://docs.angularjs.org/guide/dev_guide.e2e-testing */
+var BabitchGamePage = function(browser) {
+	browser.get(browser.baseUrl + '/?nobackend');
+
+	this.startButton = $('#actionStart button');
+	this.playersLocation = $$('.seat-layer');
+	this.score = $('.score');
+	this.firstTeamScore = $$('.score strong').get(0);
+	this.secondTeamScore = $$('.score strong').get(1);
+
+	this.getPlayerLocation = function(locationIndex) {
+		return new PlayerLocationElement(browser, $$('#match .seat').get(locationIndex));
+	};
+
+	this.getCancelLastGoalButton = function() {
+		$('.action .btn-group button').click();
+
+		return $('.cancel-last-goal');
+	};
+};
+
+var PlayerLocationElement = function(browser, playerLocation) {
+	this.playerLocation = playerLocation;
+
+	this.click = function() {
+		this.playerLocation.findElement(by.css('.seat-layer')).click();
+	};
+
+	this.selectPlayer = function(playerIndex) {
+		this.click();
+		$$('.player-list div a').get(playerIndex).click();
+	};
+
+	this.getPlayerName = function() {
+		return this.playerLocation.findElement(by.css('.player-name big')).getText();
+	};
+
+	this.goal = function() {
+		this.click();
+		$('.button.goal:not(.ng-hide)').click();
+	};
+
+	this.autogoal = function() {
+		this.click();
+		$('.button.autogoal:not(.ng-hide)').click();
+	};
+};
+
+var page = null;
 
 describe('Babitch : Choose player', function() {
 
 	beforeEach(function() {
-		browser().navigateTo('/?nobackend');
+		page = new BabitchGamePage(browser);
 	});
 
-
-	it('should automatically redirect to /', function() {
-		expect(browser().location().url()).toBe("/");
-	});
-
-	it('should have a start button', function() {
-		expect(element('#actionStart button').text()).toMatch('Start');
-		expect(element('#actionStart').css('display')).toMatch('none');
-
+	it('should not display startButton', function() {
+		expect(page.startButton.isDisplayed()).toBe(false);
 	});
 
 	it('should have 4 selectable players', function() {
-		expect(element('.player-name').count()).toBe(4);
+		expect(page.playersLocation.count()).toBe(4);
 	});
 
-	it('should not begin game without less than 4 selected players', function() {
-		element('#match .seat-layer:eq(0)').click();
-		element('.player-list div a:eq(0)').click();
-		expect(element('.player-name:eq(0) big').text()).toBe('Adrien');
-  		expect(element('.score').css('display')).toBe('none');
-
-  		expect(element('#actionStart').css('display')).toMatch('none');
+	it('should not display startButton without less than 4 selected players', function() {
+		var playerLocation = page.getPlayerLocation(0);
+		playerLocation.selectPlayer(0);
+		expect(playerLocation.getPlayerName()).toBe('Adrien');
+  		expect(page.startButton.isDisplayed()).toBe(false);
 	});
 
 	it('should begin game with 4 different selected players', function() {
-		element('#match .seat-layer:eq(0)').click();
-		element('.player-list div a:eq(0)').click();
-		expect(element('.player-name:eq(0) big').text()).toBe('Adrien');
+		var playerLocation1 = page.getPlayerLocation(0);
+		playerLocation1.selectPlayer(0);
+		expect(playerLocation1.getPlayerName()).toBe('Adrien');
 
-		element('#match .seat-layer:eq(1)').click();
-		element('.player-list div a:eq(1)').click();
-		expect(element('.player-name:eq(1) big').text()).toBe('Antony');
+		var playerLocation2 = page.getPlayerLocation(1);
+		playerLocation2.selectPlayer(1);
+		expect(playerLocation2.getPlayerName()).toBe('Antony');
 
-		element('#match .seat-layer:eq(2)').click();
-		element('.player-list div a:eq(2)').click();
-		expect(element('.player-name:eq(2) big').text()).toBe('Aurelian');
+		var playerLocation3 = page.getPlayerLocation(2);
+		playerLocation3.selectPlayer(2);
+		expect(playerLocation3.getPlayerName()).toBe('Aurelian');
 
-		element('#match .seat-layer:eq(3)').click();
-		element('.player-list div a:eq(3)').click();
-		expect(element('.player-name:eq(3) big').text()).toBe('Benjamin');
+		var playerLocation3 = page.getPlayerLocation(3);
+		playerLocation3.selectPlayer(3);
+		expect(playerLocation3.getPlayerName()).toBe('Benjamin');
 
 
-  		expect(element('#actionStart').css('display')).toMatch('block');
+  		expect(page.startButton.isDisplayed()).toBe(true);
 
 		//Ugly $timeout e2e issue here :/
-  		element('#actionStart button').click();
+  		page.startButton.click();
 
-  		expect(element('.score').css('display')).toBe('block');
+  		expect(page.score.isDisplayed()).toBe(true);
 
   		// et de 0-0
-  		expect(element('.score strong:eq(0)').text()).toBe('0');
-  		expect(element('.score strong:eq(1)').text()).toBe('0');
+  		expect(page.firstTeamScore.getText()).toBe('0');
+  		expect(page.secondTeamScore.getText()).toBe('0');
 	});
 });
 
 describe('Babitch : Game', function() {
 
 	beforeEach(function() {
-		browser().navigateTo('/?nobackend');
-		
+		page = new BabitchGamePage(browser);
+
 		//Choose 4 players
-		element('#match .seat-layer:eq(0)').click();
-		element('.player-list div a:eq(0)').click();
-		expect(element('.player-name:eq(0) big').text()).toBe('Adrien');
+		var playerLocation1 = page.getPlayerLocation(0);
+		playerLocation1.selectPlayer(0);
+		expect(playerLocation1.getPlayerName()).toBe('Adrien');
 
-		element('#match .seat-layer:eq(1)').click();
-		element('.player-list div a:eq(1)').click();
-		expect(element('.player-name:eq(1) big').text()).toBe('Antony');
+		var playerLocation2 = page.getPlayerLocation(1);
+		playerLocation2.selectPlayer(1);
+		expect(playerLocation2.getPlayerName()).toBe('Antony');
 
-		element('#match .seat-layer:eq(2)').click();
-		element('.player-list div a:eq(2)').click();
-		expect(element('.player-name:eq(2) big').text()).toBe('Aurelian');
+		var playerLocation3 = page.getPlayerLocation(2);
+		playerLocation3.selectPlayer(2);
+		expect(playerLocation3.getPlayerName()).toBe('Aurelian');
 
-		element('#match .seat-layer:eq(3)').click();
-		element('.player-list div a:eq(3)').click();
-		expect(element('.player-name:eq(3) big').text()).toBe('Benjamin');
+		var playerLocation3 = page.getPlayerLocation(3);
+		playerLocation3.selectPlayer(3);
+		expect(playerLocation3.getPlayerName()).toBe('Benjamin');
 
 		//Begin a match
-		element('#actionStart button').click();
+		page.startButton.click();
 	});
 
 	//Defense goal
 	it('should add a goal for the blue team if the blue defender goal',function() {
-		element('#match .seat-layer:eq(3)').click();
-		element('.button:eq(0)').click();
-  		expect(element('.score strong:eq(0)').text()).toBe('0');
-  		expect(element('.score strong:eq(1)').text()).toBe('1');
+		var playerLocation = page.getPlayerLocation(3);
+		playerLocation.goal();
+  		expect(page.firstTeamScore.getText()).toBe('0');
+  		expect(page.secondTeamScore.getText()).toBe('1');
 	});
 
 	it('should add a goal for the red team if the red defender goal',function() {
-		element('#match .seat-layer:eq(0)').click();
-		element('.button:eq(0)').click();
-  		expect(element('.score strong:eq(0)').text()).toBe('1');
-  		expect(element('.score strong:eq(1)').text()).toBe('0');
+		var playerLocation = page.getPlayerLocation(0);
+		playerLocation.goal();
+  		expect(page.firstTeamScore.getText()).toBe('1');
+  		expect(page.secondTeamScore.getText()).toBe('0');
 	});
 
 	//Defense CSC
 	it('should add a goal for red team if the blue defender goal csc',function() {
-		element('#match .seat-layer:eq(3)').click();
-		element('.button:eq(2)').click();
-  		expect(element('.score strong:eq(0)').text()).toBe('1');
-  		expect(element('.score strong:eq(1)').text()).toBe('0');
+		var playerLocation = page.getPlayerLocation(3);
+		playerLocation.autogoal();
+  		expect(page.firstTeamScore.getText()).toBe('1');
+  		expect(page.secondTeamScore.getText()).toBe('0');
 	});
 
 	it('should add a goal for blue team if the red defender goal csc',function() {
-		element('#match .seat-layer:eq(1)').click();
-		element('.button:eq(2)').click();
-  		expect(element('.score strong:eq(0)').text()).toBe('0');
-  		expect(element('.score strong:eq(1)').text()).toBe('1');
+		var playerLocation = page.getPlayerLocation(1);
+		playerLocation.autogoal();
+  		expect(page.firstTeamScore.getText()).toBe('0');
+  		expect(page.secondTeamScore.getText()).toBe('1');
 	});
 
 	//Attack Goal
 	it('should add a goal for the blue team if the blue attacker goal',function() {
-		element('#match .seat-layer:eq(2)').click();
-		element('.button:eq(0)').click();
-  		expect(element('.score strong:eq(0)').text()).toBe('0');
-  		expect(element('.score strong:eq(1)').text()).toBe('1');
+		var playerLocation = page.getPlayerLocation(2);
+		playerLocation.goal();
+  		expect(page.firstTeamScore.getText()).toBe('0');
+  		expect(page.secondTeamScore.getText()).toBe('1');
 	});
 
+
 	it('should add a goal for the red team if the red attacker goal',function() {
-		element('#match .seat-layer:eq(0)').click();
-		element('.button:eq(0)').click();
-  		expect(element('.score strong:eq(0)').text()).toBe('1');
-  		expect(element('.score strong:eq(1)').text()).toBe('0');
+		var playerLocation = page.getPlayerLocation(0);
+		playerLocation.goal();
+  		expect(page.firstTeamScore.getText()).toBe('1');
+  		expect(page.secondTeamScore.getText()).toBe('0');
 	});
 
 	//Attack CSC
 	it('should add a goal for red team if the blue attacker goal csc',function() {
-		element('#match .seat-layer:eq(2)').click();
-		element('.button:eq(2)').click();
-  		expect(element('.score strong:eq(0)').text()).toBe('1');
-  		expect(element('.score strong:eq(1)').text()).toBe('0');
+		var playerLocation = page.getPlayerLocation(2);
+		playerLocation.autogoal();
+  		expect(page.firstTeamScore.getText()).toBe('1');
+  		expect(page.secondTeamScore.getText()).toBe('0');
 	});
 
 	it('should add a goal for blue team if the red attacker goal csc',function() {
-		element('#match .seat-layer:eq(1)').click();
-		element('.button:eq(2)').click();
-  		expect(element('.score strong:eq(0)').text()).toBe('0');
-  		expect(element('.score strong:eq(1)').text()).toBe('1');
+		var playerLocation = page.getPlayerLocation(1);
+		playerLocation.autogoal();
+  		expect(page.firstTeamScore.getText()).toBe('0');
+  		expect(page.secondTeamScore.getText()).toBe('1');
 	});
 
 	//Cancel a Goal
 	it('should cancel last goal',function() {
-		element('#match .seat-layer:eq(3)').click();
-		element('.button:eq(0)').click();
-  		expect(element('.score strong:eq(0)').text()).toBe('0');
-  		expect(element('.score strong:eq(1)').text()).toBe('1');
-		
-		//The "cancel last goal" button must be visible after a goal
-		expect(element('.action .btn-group ul li:eq(2) a').css('display')).toBe('block');
-		expect(element('.action .btn-group ul li:eq(2) a').text()).toMatch('Cancel last goal');
+		var playerLocation = page.getPlayerLocation(3);
+		playerLocation.goal();
+  		expect(page.firstTeamScore.getText()).toBe('0');
+  		expect(page.secondTeamScore.getText()).toBe('1');
 
 		//Cancel the goal
-		element('.action .btn-group ul li:eq(2) a').click();
-  		expect(element('.score strong:eq(0)').text()).toBe('0');
-  		expect(element('.score strong:eq(1)').text()).toBe('0');
+		var cancelLastGoalButton = page.getCancelLastGoalButton();
+		expect(cancelLastGoalButton.isDisplayed()).toBe(true);
+		cancelLastGoalButton.click();
 
   		//The button must be hidden
-		expect(element('.action .btn-group ul li:eq(1)').css('display')).toBe('none');
+  		expect(page.firstTeamScore.getText()).toBe('0');
+  		expect(page.secondTeamScore.getText()).toBe('0');
+  		expect(cancelLastGoalButton.isDisplayed()).toBe(false);
 	});
 
 });
