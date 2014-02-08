09f21006156ccfee62c4a220888a46f0cbdcba9f
Kenny Dits <kenny.dits@m6.fr>
2014-02-08 17:20:23 +0100

    init controller test for team value

---
+++ test/spec/controllers/babitchStats.js
@@ -0,0 +1,589 @@
+//Fixtures
+
+var JsonGames = [{
+    "id": 252,
+    "blue_score": 10,
+    "red_score": 9,
+    "composition": [{
+        "player_id": 16,
+        "id": 1005,
+        "team": "red",
+        "position": "defense"
+    }, {
+        "player_id": 8,
+        "id": 1006,
+        "team": "blue",
+        "position": "attack"
+    }, {
+        "player_id": 9,
+        "id": 1007,
+        "team": "red",
+        "position": "attack"
+    }, {
+        "player_id": 12,
+        "id": 1008,
+        "team": "blue",
+        "position": "defense"
+    }],
+    "goals": [{
+        "player_id": 12,
+        "conceder_id": 12,
+        "id": 3969,
+        "position": "defense",
+        "autogoal": true
+    }, {
+        "player_id": 9,
+        "conceder_id": 12,
+        "id": 3970,
+        "position": "attack",
+        "autogoal": false
+    }, {
+        "player_id": 9,
+        "conceder_id": 12,
+        "id": 3971,
+        "position": "attack",
+        "autogoal": false
+    }, {
+        "player_id": 9,
+        "conceder_id": 12,
+        "id": 3972,
+        "position": "attack",
+        "autogoal": false
+    }, {
+        "player_id": 8,
+        "conceder_id": 16,
+        "id": 3973,
+        "position": "attack",
+        "autogoal": false
+    }, {
+        "player_id": 12,
+        "conceder_id": 16,
+        "id": 3974,
+        "position": "defense",
+        "autogoal": false
+    }, {
+        "player_id": 8,
+        "conceder_id": 16,
+        "id": 3975,
+        "position": "attack",
+        "autogoal": false
+    }, {
+        "player_id": 12,
+        "conceder_id": 16,
+        "id": 3976,
+        "position": "defense",
+        "autogoal": false
+    }, {
+        "player_id": 9,
+        "conceder_id": 12,
+        "id": 3977,
+        "position": "attack",
+        "autogoal": false
+    }, {
+        "player_id": 8,
+        "conceder_id": 16,
+        "id": 3978,
+        "position": "attack",
+        "autogoal": false
+    }, {
+        "player_id": 9,
+        "conceder_id": 12,
+        "id": 3979,
+        "position": "attack",
+        "autogoal": false
+    }, {
+        "player_id": 8,
+        "conceder_id": 16,
+        "id": 3980,
+        "position": "attack",
+        "autogoal": false
+    }, {
+        "player_id": 9,
+        "conceder_id": 12,
+        "id": 3981,
+        "position": "attack",
+        "autogoal": false
+    }, {
+        "player_id": 8,
+        "conceder_id": 16,
+        "id": 3982,
+        "position": "attack",
+        "autogoal": false
+    }, {
+        "player_id": 16,
+        "conceder_id": 12,
+        "id": 3983,
+        "position": "defense",
+        "autogoal": false
+    }, {
+        "player_id": 8,
+        "conceder_id": 16,
+        "id": 3984,
+        "position": "attack",
+        "autogoal": false
+    }, {
+        "player_id": 9,
+        "conceder_id": 12,
+        "id": 3985,
+        "position": "attack",
+        "autogoal": false
+    }, {
+        "player_id": 16,
+        "conceder_id": 16,
+        "id": 3986,
+        "position": "defense",
+        "autogoal": true
+    }, {
+        "player_id": 12,
+        "conceder_id": 16,
+        "id": 3987,
+        "position": "defense",
+        "autogoal": false
+    }]
+}, {
+    "id": 251,
+    "blue_score": 10,
+    "red_score": 3,
+    "composition": [{
+        "player_id": 12,
+        "id": 1001,
+        "team": "red",
+        "position": "defense"
+    }, {
+        "player_id": 8,
+        "id": 1002,
+        "team": "blue",
+        "position": "attack"
+    }, {
+        "player_id": 7,
+        "id": 1003,
+        "team": "red",
+        "position": "attack"
+    }, {
+        "player_id": 9,
+        "id": 1004,
+        "team": "blue",
+        "position": "defense"
+    }],
+    "goals": [{
+        "player_id": 8,
+        "conceder_id": 7,
+        "id": 3956,
+        "position": "attack",
+        "autogoal": false
+    }, {
+        "player_id": 9,
+        "conceder_id": 7,
+        "id": 3957,
+        "position": "defense",
+        "autogoal": false
+    }, {
+        "player_id": 12,
+        "conceder_id": 9,
+        "id": 3958,
+        "position": "attack",
+        "autogoal": false
+    }, {
+        "player_id": 9,
+        "conceder_id": 7,
+        "id": 3959,
+        "position": "defense",
+        "autogoal": false
+    }, {
+        "player_id": 8,
+        "conceder_id": 7,
+        "id": 3960,
+        "position": "attack",
+        "autogoal": false
+    }, {
+        "player_id": 8,
+        "conceder_id": 7,
+        "id": 3961,
+        "position": "attack",
+        "autogoal": false
+    }, {
+        "player_id": 12,
+        "conceder_id": 9,
+        "id": 3962,
+        "position": "attack",
+        "autogoal": false
+    }, {
+        "player_id": 9,
+        "conceder_id": 7,
+        "id": 3963,
+        "position": "defense",
+        "autogoal": false
+    }, {
+        "player_id": 12,
+        "conceder_id": 9,
+        "id": 3964,
+        "position": "defense",
+        "autogoal": false
+    }, {
+        "player_id": 8,
+        "conceder_id": 12,
+        "id": 3965,
+        "position": "attack",
+        "autogoal": false
+    }, {
+        "player_id": 8,
+        "conceder_id": 12,
+        "id": 3966,
+        "position": "attack",
+        "autogoal": false
+    }, {
+        "player_id": 8,
+        "conceder_id": 12,
+        "id": 3967,
+        "position": "attack",
+        "autogoal": false
+    }, {
+        "player_id": 8,
+        "conceder_id": 12,
+        "id": 3968,
+        "position": "attack",
+        "autogoal": false
+    }]
+}, {
+    "id": 250,
+    "blue_score": 10,
+    "red_score": 6,
+    "composition": [{
+        "player_id": 7,
+        "id": 997,
+        "team": "red",
+        "position": "defense"
+    }, {
+        "player_id": 9,
+        "id": 998,
+        "team": "blue",
+        "position": "attack"
+    }, {
+        "player_id": 16,
+        "id": 999,
+        "team": "red",
+        "position": "attack"
+    }, {
+        "player_id": 8,
+        "id": 1000,
+        "team": "blue",
+        "position": "defense"
+    }],
+    "goals": [{
+        "player_id": 8,
+        "conceder_id": 16,
+        "id": 3940,
+        "position": "defense",
+        "autogoal": false
+    }, {
+        "player_id": 9,
+        "conceder_id": 16,
+        "id": 3941,
+        "position": "attack",
+        "autogoal": false
+    }, {
+        "player_id": 9,
+        "conceder_id": 16,
+        "id": 3942,
+        "position": "attack",
+        "autogoal": false
+    }, {
+        "player_id": 8,
+        "conceder_id": 16,
+        "id": 3943,
+        "position": "defense",
+        "autogoal": false
+    }, {
+        "player_id": 7,
+        "conceder_id": 8,
+        "id": 3944,
+        "position": "attack",
+        "autogoal": false
+    }, {
+        "player_id": 8,
+        "conceder_id": 16,
+        "id": 3945,
+        "position": "defense",
+        "autogoal": false
+    }, {
+        "player_id": 8,
+        "conceder_id": 8,
+        "id": 3946,
+        "position": "defense",
+        "autogoal": true
+    }, {
+        "player_id": 9,
+        "conceder_id": 16,
+        "id": 3947,
+        "position": "attack",
+        "autogoal": false
+    }, {
+        "player_id": 8,
+        "conceder_id": 8,
+        "id": 3948,
+        "position": "defense",
+        "autogoal": true
+    }, {
+        "player_id": 16,
+        "conceder_id": 8,
+        "id": 3949,
+        "position": "attack",
+        "autogoal": false
+    }, {
+        "player_id": 9,
+        "conceder_id": 7,
+        "id": 3950,
+        "position": "attack",
+        "autogoal": false
+    }, {
+        "player_id": 16,
+        "conceder_id": 8,
+        "id": 3951,
+        "position": "attack",
+        "autogoal": false
+    }, {
+        "player_id": 16,
+        "conceder_id": 8,
+        "id": 3952,
+        "position": "attack",
+        "autogoal": false
+    }, {
+        "player_id": 8,
+        "conceder_id": 7,
+        "id": 3953,
+        "position": "defense",
+        "autogoal": false
+    }, {
+        "player_id": 9,
+        "conceder_id": 7,
+        "id": 3954,
+        "position": "attack",
+        "autogoal": false
+    }, {
+        "player_id": 9,
+        "conceder_id": 7,
+        "id": 3955,
+        "position": "attack",
+        "autogoal": false
+    }]
+}];
+
+var JsonPlayer = [{
+    "id": 21,
+    "name": "Nicolas B",
+    "email": "nicolas.b"
+}, {
+    "id": 20,
+    "name": "stephanie",
+    "email": "stephanie"
+}, {
+    "id": 19,
+    "name": "Damien Jv",
+    "email": "Damien"
+}, {
+    "id": 18,
+    "name": "JulienB",
+    "email": "Jbes"
+}, {
+    "id": 17,
+    "name": "Jean micheng",
+    "email": "jm"
+}, {
+    "id": 16,
+    "name": "Gregory",
+    "email": "mail"
+}, {
+    "id": 15,
+    "name": "Pierre Thomas",
+    "email": "mail"
+}, {
+    "id": 14,
+    "name": "Benjamin",
+    "email": "br"
+}, {
+    "id": 13,
+    "name": "Antony",
+    "email": "ap"
+}, {
+    "id": 12,
+    "name": "Aurelian",
+    "email": "abe"
+}, {
+    "id": 11,
+    "name": "Marc",
+    "email": "marc"
+}, {
+    "id": 10,
+    "name": "R\u00e9mi",
+    "email": "rle"
+}, {
+    "id": 9,
+    "name": "Kenny",
+    "email": "kenny"
+}, {
+    "id": 8,
+    "name": "Nicolas C",
+    "email": "nc"
+}, {
+    "id": 7,
+    "name": "Florent L",
+    "email": "fl"
+}, {
+    "id": 6,
+    "name": "J\u00e9r\u00e9my",
+    "email": "c"
+}, {
+    "id": 5,
+    "name": "Francis",
+    "email": "cy"
+}, {
+    "id": 4,
+    "name": "Morgan",
+    "email": "b"
+}, {
+    "id": 3,
+    "name": "Florent",
+    "email": "c"
+}, {
+    "id": 2,
+    "name": "Denis",
+    "email": "denis"
+}, {
+    "id": 1,
+    "name": "Adrien",
+    "email": "adrien"
+}];
+
+
+describe('Controller: BabitchStatsCtrl', function() {
+
+    // load the controller's module
+    beforeEach(module('babitchFrontendApp'));
+    var theBabitchStatsCtrl,
+        scope,
+        httpMock,
+        config;
+
+    // Initialize the controller and a mock scope
+    beforeEach(inject(function($controller, $rootScope, $httpBackend, CONFIG) {
+        scope = $rootScope.$new();
+        httpMock = $httpBackend;
+        config = CONFIG;
+
+        httpMock.whenGET(config.BABITCH_WS_URL + "/players").respond(JsonPlayer);
+        httpMock.whenGET(config.BABITCH_WS_URL + "/games?per_page=100").respond(JsonGames);
+
+        theBabitchStatsCtrl = $controller('babitchStatsCtrl', {
+            $scope: scope
+        });
+
+        //Flush the .query
+        httpMock.flush();
+
+    }));
+
+    afterEach(function() {
+        httpMock.verifyNoOutstandingExpectation();
+        httpMock.verifyNoOutstandingRequest();
+    });
+
+    it('should load all players', function() {
+        expect(scope.playersList.length).toBe(22);
+    });
+
+    it('should load all games', function() {
+        expect(scope.gamesList.length).toBe(3);
+    });
+
+    it('should generate stats for all players', function() {
+        expect(scope.statsPlayers.length).toBe(22);
+    });
+
+    it('should generate stats for all teams', function() {
+        expect(scope.statsTeams.length).toBe(5); //5 different teams
+    });
+
+    it('should list correct match', function() {
+        expect(scope.gamesList[0].blue_score).toBe(10);
+        expect(scope.gamesList[0].red_score).toBe(9);
+
+        expect(scope.gamesList[1].blue_score).toBe(10);
+        expect(scope.gamesList[1].red_score).toBe(3);
+
+        expect(scope.gamesList[2].blue_score).toBe(10);
+        expect(scope.gamesList[2].red_score).toBe(6);
+        //48 goals
+    });
+
+    it('should calculate team victory correctly', function() {
+        expect(scope.statsTeams.length).toBe(5); //5 different teams
+        expect(scope.statsTeams[0].victory).toBe(0);
+        expect(scope.statsTeams[1].victory).toBe(1);
+        expect(scope.statsTeams[2].victory).toBe(0);
+        expect(scope.statsTeams[3].victory).toBe(2);
+        expect(scope.statsTeams[4].victory).toBe(0);
+    });
+
+    it('should calculate team loose correctly', function() {
+        expect(scope.statsTeams[0].loose).toBe(1);
+        expect(scope.statsTeams[1].loose).toBe(0);
+        expect(scope.statsTeams[2].loose).toBe(1);
+        expect(scope.statsTeams[3].loose).toBe(0);
+        expect(scope.statsTeams[4].loose).toBe(1);
+    });
+
+    it('should calculate team percentVictory correctly', function() {
+        expect(scope.statsTeams[0].percentVictory).toBe(0);
+        expect(scope.statsTeams[1].percentVictory).toBe(100);
+        expect(scope.statsTeams[2].percentVictory).toBe(0);
+        expect(scope.statsTeams[3].percentVictory).toBe(100);
+        expect(scope.statsTeams[4].percentVictory).toBe(0);
+    });
+
+    it('should calculate team percentLoose correctly', function() {
+        expect(scope.statsTeams[0].percentLoose).toBe(100);
+        expect(scope.statsTeams[1].percentLoose).toBe(0);
+        expect(scope.statsTeams[2].percentLoose).toBe(100);
+        expect(scope.statsTeams[3].percentLoose).toBe(0);
+        expect(scope.statsTeams[4].percentLoose).toBe(100);
+    });
+
+    it('should calculate team teamGoalaverage correctly', function() {
+        expect(scope.statsTeams[0].teamGoalaverage).toBe(-2);
+        expect(scope.statsTeams[1].teamGoalaverage).toBe(2);
+        expect(scope.statsTeams[2].teamGoalaverage).toBe(-14);
+        expect(scope.statsTeams[3].teamGoalaverage).toBe(22);
+        expect(scope.statsTeams[4].teamGoalaverage).toBe(-8);
+    });
+
+    it('should calculate team goal correctly', function() {
+        expect(scope.statsTeams[0].goal).toBe(8);
+        expect(scope.statsTeams[1].goal).toBe(9);
+        expect(scope.statsTeams[2].goal).toBe(3);
+        expect(scope.statsTeams[3].goal).toBe(20);
+        expect(scope.statsTeams[4].goal).toBe(4);
+    });
+
+    it('should calculate team owngoal correctly', function() {
+        expect(scope.statsTeams[0].owngoal).toBe(1);
+        expect(scope.statsTeams[1].owngoal).toBe(1);
+        expect(scope.statsTeams[2].owngoal).toBe(0);
+        expect(scope.statsTeams[3].owngoal).toBe(2);
+        expect(scope.statsTeams[4].owngoal).toBe(0);
+    });
+
+    it('should do not forget one goal', function() {
+        var nbGoal = scope.statsTeams[0].goal
+            +scope.statsTeams[1].goal
+            +scope.statsTeams[2].goal
+            +scope.statsTeams[3].goal
+            +scope.statsTeams[4].goal
+            +scope.statsTeams[0].owngoal
+            +scope.statsTeams[1].owngoal
+            +scope.statsTeams[2].owngoal
+            +scope.statsTeams[3].owngoal
+            +scope.statsTeams[4].owngoal;
+        expect(nbGoal).toBe(48);
+    });
+});
+
