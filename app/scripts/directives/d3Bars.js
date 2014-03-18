f2bb690604457c61647fd84b19a27d4dbddd756c
Kenny Dits <kenny.dits@m6.fr>
2014-03-18 06:11:12 +0100

    Fix coding style issue

--- app/scripts/directives/d3Bars.js
+++ app/scripts/directives/d3Bars.js
@@ -13,13 +13,13 @@ function($window, $timeout, d3Service, gravatarService) {
             d3Service.d3().then(function(d3) {
 
                 var renderTimeout;
-                var margin = parseInt(attrs.margin) || 20,
-                barHeight = parseInt(attrs.barHeight) || 20,
-                barPadding = parseInt(attrs.barPadding) || 5;
+                var margin = parseInt(attrs.margin, 10) || 20;
+                var barHeight = parseInt(attrs.barHeight, 10) || 20;
+                var barPadding = parseInt(attrs.barPadding, 10) || 5;
 
                 var svg = d3.select(ele[0])
-                .append('svg')
-                .style('width', '100%');
+                    .append('svg')
+                    .style('width', '100%');
 
                 //Re-render on resize
                 $window.onresize = function() {
@@ -49,7 +49,9 @@ function($window, $timeout, d3Service, gravatarService) {
 
                         //Deal with bar width
                         var width = d3.select(ele[0])[0][0].offsetWidth - margin - RectBegin - RectEnd;
-                        if (width<0) return;
+                        if (width < 0) {
+                            return;
+                        }
 
                         //Deal with bar height
                         var height = scope.data.length * (barHeight + barPadding);
@@ -72,106 +74,104 @@ function($window, $timeout, d3Service, gravatarService) {
 
                         //Tell D3 (min/max) range of the dataset
                         var xScale = d3.scale.linear()
-                        .domain([d3min, d3max])
-                        .range([0, width]);
+                            .domain([d3min, d3max])
+                            .range([0, width]);
 
                         //First, draw all rect (bars)
                         svg.selectAll('rect')
-                        .data(d3.values(data))
-                        .enter()
-                        .append('rect')
-                        .on('click', function(d,i) {
-                            return scope.onClick({item: d});
-                        })
-                        .attr('height', barHeight)
-                        .attr('width', 140)
-                        .attr('x', RectBegin + Math.round(margin/2))
-                        .attr('y', function(d,i) {
-                            return i * (barHeight + barPadding);
-                        })
-                        .attr('fill', function(d) {
-                            return color(d.stat);
-                        })
-                        .transition()
-                        .duration(1000)
-                        .attr('width', function(d) {
-                            if(typeof d.stat == "number") {
+                            .data(d3.values(data))
+                            .enter()
+                            .append('rect')
+                            .on('click', function(d,i) {
+                                return scope.onClick({item: d});
+                            })
+                            .attr('height', barHeight)
+                            .attr('width', 140)
+                            .attr('x', RectBegin + Math.round(margin/2))
+                            .attr('y', function(d,i) {
+                                return i * (barHeight + barPadding);
+                            })
+                            .attr('fill', function(d) {
+                                return color(d.stat);
+                            })
+                            .transition()
+                            .duration(1000)
+                            .attr('width', function(d) {
                                 return xScale(d.stat);
-                            }
-                            else return 0;
-                        });
+                            });
 
                         //Then draw all Gravatar
                         var gimgs = svg.selectAll("g")
-                        .data(data)
-                        .enter().append('g');
+                            .data(data)
+                            .enter().append('g');
 
                         var imgs = gimgs.selectAll("image")
-                        .data(function(d) {
-                            return d.players;
-                        })
-                        .enter()
-                        .append("svg:image")
-                        .attr("xlink:href", function(d) {
-                            return gravatarService.url(d.email);
-                        })
-                        .attr("x", function(d,iPlayers,iStat) {
-                            var x = RectBegin - barHeight - barPadding + Math.round(margin/2);
-                            if(iPlayers == 1) {
-                                x = x - barHeight - barPadding;
-                            }
-                            return x;
-                        })
-                        .attr('y', function(d,iPlayers,iStat) {
-                            return iStat * (barHeight + barPadding);
-                        })
-                        .attr("width", barHeight)
-                        .attr("height", barHeight);
+                            .data(function(d) {
+                                return d.players;
+                            })
+                            .enter()
+                            .append("svg:image")
+                            .attr("xlink:href", function(d) {
+                                return gravatarService.url(d.email);
+                            })
+                            .attr("x", function(d,iPlayers,iStat) {
+                                var x = RectBegin - barHeight - barPadding + Math.round(margin/2);
+                                if(iPlayers == 1) {
+                                    x = x - barHeight - barPadding;
+                                }
+                                return x;
+                            })
+                            .attr('y', function(d,iPlayers,iStat) {
+                                return iStat * (barHeight + barPadding);
+                            })
+                            .attr("width", barHeight)
+                            .attr("height", barHeight);
 
                         //Finally add player(s) name
                         var texts = svg.selectAll('text')
-                        .data(d3.values(data));
-                        texts.enter()
-                        .append('text')
-                        .attr('fill', '#000')
-                        .attr('y', function(d,i) {
-                            return i * (barHeight + barPadding) + 15;
-                        })
-                        .attr("x", function(d,iPlayers,iStat) {
-                            var x = RectBegin - barHeight;
-                            if(scope.type == "teams") x = x - barHeight - barPadding;
-                            return x;
-                        })
-                        .attr("text-anchor", "end")
-                        .text(function(d) {
-                            var text = '';
-                            d.players.forEach(function(player) {
-                                if(text) text = ' - ' + text;
-                                text = player.name + text;
+                            .data(d3.values(data));
+                            texts.enter()
+                            .append('text')
+                            .attr('fill', '#000')
+                            .attr('y', function(d,i) {
+                                return i * (barHeight + barPadding) + 15;
+                            })
+                            .attr("x", function(d,iPlayers,iStat) {
+                                var x = RectBegin - barHeight;
+                                if(scope.type == "teams") {
+                                    x = x - barHeight - barPadding;
+                                }
+                                return x;
+                            })
+                            .attr("text-anchor", "end")
+                            .text(function(d) {
+                                var text = '';
+                                d.players.forEach(function(player) {
+                                    if(text) {
+                                        text = ' - ' + text;
+                                    }
+                                    text = player.name + text;
+                                });
+                                return text;
                             });
-                            return text;
-                        });
 
                         //Add stats value to the right
                         texts.enter()
-                        .append('text')
-                        .attr('fill', '#000')
-                        .attr('y', function(d,i) {
-                            return i * (barHeight + barPadding) + 15;
-                        })
-                        .attr('x', 140 + RectBegin + barPadding + barPadding)
-                        .attr("text-anchor", "start")
-                        .text(function(d) {
-                            return (typeof d.stat == "undefined" ? "" : d.stat);
-                        })
-                        .transition()
-                        .duration(950)
-                        .attr('x',function(d) {
-                            if(typeof d.stat == "number") {
+                            .append('text')
+                            .attr('fill', '#000')
+                            .attr('y', function(d,i) {
+                                return i * (barHeight + barPadding) + 15;
+                            })
+                            .attr('x', 140 + RectBegin + barPadding + barPadding)
+                            .attr("text-anchor", "start")
+                            .text(function(d) {
+                                return d.stat;
+                            })
+                            .transition()
+                            .duration(950)
+                            .attr('x',function(d) {
                                 return xScale(d.stat) + RectBegin + barPadding + barPadding;
-                            }
-                            else return 0;
-                        });
+                            });
 
                     }, 200);
                 };
--- app/scripts/services/d3.js
+++ app/scripts/services/d3.js
@@ -12,7 +12,7 @@ angular.module('babitchFrontendApp')
       var scriptTag = $document[0].createElement('script');
       scriptTag.type = 'text/javascript';
       scriptTag.async = true;
-      scriptTag.src = 'http://d3js.org/d3.v3.min.js';
+      scriptTag.src = '/bower_components/d3js/build/d3.v3.min.js';
       scriptTag.onreadystatechange = function () {
         if (this.readyState == 'complete') onScriptLoad();
       }
