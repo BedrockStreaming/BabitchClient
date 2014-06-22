861c8258c95a4d55b5ea4949664af0e685afb410
Jérémy Jourdin <jeremy.jourdin@free.fr>
2014-06-22 22:26:49 +0200

    Fix travis

---
+++ docker/travis.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Exit on first error
+set -e
+
+# Kill background processes on exit
+trap 'kill $(jobs -p)' SIGINT SIGTERM EXIT
+
+# Start docker daemon
+docker -d &
+sleep 1
+
+# Use docker
+docker build -t babitch_client_travis .
+docker run --privileged -it --rm babitch_client_travis test
