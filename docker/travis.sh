20c50322aba7b6daa6d28291d6cc842413559e04
Jérémy Jourdin <jeremy.jourdin@free.fr>
2014-06-23 19:33:15 +0200

    Fix travis

--- docker/travis.sh
+++ docker/travis.sh
@@ -3,15 +3,19 @@
 # Exit on first error
 set -e
 
+# Mount rount
+mount -t tmpfs none /root
+
 # Kill background processes on exit
 trap 'kill $(jobs -p)' SIGINT SIGTERM EXIT
 
+# Workaround with cgroup for recent version of docker
+cgroups-umount
+cgroups-mount
+
 # Start docker daemon
 docker -d &
-sleep 1
-
-echo $1
-ls -l $1
+sleep 2
 
 # Use docker
 docker build -t babitch_client_travis $1
